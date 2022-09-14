1421715f9b1a560f59e4cd4481bcaa3f9686a389
kozyrek <kozyrevaia@bk.ru>
2022-09-15 01:32:13 +0500

    правит замечания после тестирования

--- source/js/modules/init-slider.js
+++ source/js/modules/init-slider.js
@@ -1,8 +1,8 @@
 const sliderTrainers = document.querySelector('.swiper.trainers__slider');
 const buttonPrev = document.querySelector('.swiper-button-prev.trainers__button-prev');
 const buttonNext = document.querySelector('.swiper-button-next.trainers__button-next');
-// const DESKTOP_WIDTH = 1200;
-// const TABLET_WIDTH = 768;
+const DESKTOP_WIDTH = 1200;
+const TABLET_WIDTH = 768;
 
 const slider = new window.Swiper(sliderTrainers, {
   slidesPerView: 4,
@@ -33,35 +33,24 @@ const slider = new window.Swiper(sliderTrainers, {
   watchOverflow: true,
 });
 
-// const changeDirection = (item) => {
-//   if (window.screen.width >= DESKTOP_WIDTH) {
-//     item.activeIndex = 4;
-//   } else if (window.screen.width < DESKTOP_WIDTH && window.screen.width >= (TABLET_WIDTH - 1)) {
-//     item.activeIndex = 4;
-//   } else if (window.screen.width < (TABLET_WIDTH - 1)) {
-//     item.activeIndex = 3;
-//   }
-// };
+const changeDirection = (item) => {
+  if (window.screen.width >= DESKTOP_WIDTH) {
+    item.activeIndex = 4;
+  } else if (window.screen.width < DESKTOP_WIDTH && window.screen.width >= (TABLET_WIDTH - 1)) {
+    item.activeIndex = 4;
+  } else if (window.screen.width < (TABLET_WIDTH - 1)) {
+    item.activeIndex = 3;
+  }
+};
 
-// window.addEventListener('resize', () => {
-//   if (slider) {
-//     changeDirection(slider);
-//   }
-// });
+window.addEventListener('resize', () => {
+  if (slider) {
+    changeDirection(slider);
+  }
+});
 
 const initSlider = () => {
   return slider;
 };
 
 export {initSlider};
-
-// spaceBetween: 0,////
-//   slidesPerView: 1,
-// direction: 'horizontal',
-// simulateTouch: true,
-// grabCursor: true,
-// slideToClickedSlide: true,
-// keyboard: {
-//   enabled: true,
-//   onlyInViewport: true,
-//   pageUpDown: true
